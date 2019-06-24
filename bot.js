2c9f826d41f577291c087bc661690a3ff7c24b1b
abdosami1500 <44530940+abdosami1500@users.noreply.github.com>
2019-06-24 19:07:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,6 @@ client.on("ready", () => {
 let channel =     client.channels.get("592416949796077570")
 setInterval(function() {
 channel.send(`abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo `);
-}, 25)
+}, 9999999999999)
 })
 client.login(process.env.BOT_TOKEN);
