a3c7b752f6bb250d98aaad2bbd8a577abf5206bc
abdosami1500 <44530940+abdosami1500@users.noreply.github.com>
2019-06-24 23:42:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("592416949796077570")
 setInterval(function() {
-channel.send(`abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo abdo `);
-}, 9999999999999)
+channel.send(`السلام عليكم وعليكم السلام `);
+}, 25)
 })
 client.login(process.env.BOT_TOKEN);
