85c5900c913fa4a93bba0f879d98f5d8acf00e84
abdosami1500 <44530940+abdosami1500@users.noreply.github.com>
2019-06-24 03:58:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("Scrpit By Dream");
-const myid = [551422413108281345];
-const prefix = [!];
 
 client.on("ready", () => {
 let channel =     client.channels.get("592416949796077570")
@@ -10,11 +8,4 @@ setInterval(function() {
 channel.send(`السلام عليكم وعليكم السلام`);
 }, 25)
 })
-if (command == "تحدث") {
-let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
-if (!rank) return message.reply(' ')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
 client.login(process.env.BOT_TOKEN);
